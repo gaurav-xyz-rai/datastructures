7469e1f1a1ce131e2736c33f23ed98e77a743e15
Gaurav Rai <gaurav.rai@airbnb.com>
2021-06-24 21:28:17 -0700

    Modified HelloWorld Statement

--- src/com/headfirst/HelloWorld.java
+++ src/com/headfirst/HelloWorld.java
@@ -3,7 +3,7 @@ package com.headfirst;
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.println("HelloWorld in Java");
+        System.out.println("Hello, World");
     }
 
     }
