1f7feedd75890e06be2ecb25170319389ce18282
Gaurav Rai <gaurav.x.rai@gmail.com>
2022-12-11 20:25:44 -0800

    Fibonnaci Class

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import recursion.Fibonacci;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!!");
+        new Fibonacci();
     }
 }
