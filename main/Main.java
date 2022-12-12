2ad7408ee44f5fea11605298c68241f98322ed65
Gaurav Rai <gaurav.x.rai@gmail.com>
2022-12-11 21:17:28 -0800

    SumOfDigits

--- main/Main.java
+++ main/Main.java
@@ -1,12 +1,19 @@
 package main;
 
 import recursion.Fibonacci;
+import recursion.SumOfDigits;
 
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!!");
-        new Fibonacci();
+
+        /* The Recursion classes*/
+
+        System.out.println("The Fibonacci number is : " + new Fibonacci().fibonacci(-15));
+        System.out.println("The Sum of Digits are : " + new SumOfDigits().sumOfDigits(10345));
+
+
     }
 }
---
+++ recursion/SumOfDigits.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class SumOfDigits
+{
+    public int sumOfDigits(int n)
+    {
+        if (n == 0)
+        {
+            return n;
+        }
+        if (n < 0)
+        {
+            return ();
+        }
+        return (n%10 + sumOfDigits(n/10));
+    }
+}
